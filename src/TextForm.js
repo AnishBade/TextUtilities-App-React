882255c6c14abaf5737705fa45c81d5ba447a341
AnishBade <badeanish6045@gmail.com>
2023-05-04 16:54:13 +0545

    Navbar and texform created raw

---
+++ src/TextForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TextForm() {
+  return (
+    <div className='container'>
+        
+        <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+        </div>
+    </div>
+  )
+}
