d42f8d0e64909b2d4513e21cb1bac7be96d11ff1
AnishBade <badeanish6045@gmail.com>
2023-05-12 23:24:34 +0545

    Best Darkmode added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,10 +40,10 @@ export default function TextForm(props) {
     // setText("Enter your text here")
   return (
     <>
-        <div className='container' style={{color:props.mode==='light'?'black':'white'}}>
+        <div className='container' style={{color:props.mode==='light'?'#042743':'white'}}>
             <h1 >{props.heading}  </h1>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='light'?'white':'grey', color:props.mode==='light'?'black':'white'}} rows="8"></textarea>
+                <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='light'?'white':'grey', color:props.mode==='light'?'#042743':'white'}} rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
@@ -52,12 +52,12 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
 
         </div>
-        <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}> 
+        <div className="container my-3" style={{color:props.mode==='light'?'#042743':'white'}}> 
             <h1>Your text summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 *text.split(" ").length} Minutes read </p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter something in the textbox above to preview it here. "}</p>
         </div>
     </>
   )
